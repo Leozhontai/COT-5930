f0e39a6e86fa15471e129fae34f60c195c95bd82
Leozhontai <144255412+Leozhontai@users.noreply.github.com>
2025-03-29 21:33:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,8 @@ def get_file(filename):
         return abort(500, description="Error retrieving file.")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=True)
+
 
 
 
